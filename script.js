4e2032419dbd2c044adf6f35448b226a1a0a8eed
concept92 <moajz77@gmail.com>
2023-10-24 16:05:23 +0300

    Second commit

--- script.js
+++ script.js
@@ -10,5 +10,17 @@ function getMeme() {
         .catch(error => console.error("Error fetching meme:", error));
 }
 
-getMeme();
-setInterval(getMeme, 5000); // Refresh every 5 seconds
+function getMeme() {
+    fetch("https://meme-api.com/gimme")
+        .then(response => response.json())
+        .then(data => {
+            const memePic = data.preview ? data.preview[data.preview.length - 2] : "No preview available";
+            const subreddit = data.subreddit || "Unknown subreddit";
+            document.getElementById("meme-pic").src = memePic;
+            document.getElementById("subreddit").textContent = subreddit;
+        })
+        .catch(error => console.error("Error fetching meme:", error));
+}
+
+//setInterval(getMeme, 5000); // Refresh every 5 seconds
+
